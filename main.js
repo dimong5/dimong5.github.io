3bbd36456ccba7a1e36dbfc7d2e4482acd99ab03
dimong5 <dimatres666@gmail.com>
2018-06-08 12:26:43 +0300

    1

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 alert("Введите температуру в градусах Цельсия");
 var tCelsium=prompt(),
     tFarenheit=9/5*tCelsium+32,
-    a=66666666,b=3333;
+    a=66666666,b=3333, admin, name, myTest;
 alert("Температура в Фаренгейтах="+tFarenheit);
-console.log("допустим у нас есть a и b:");
-console.log("a="+a);
-console.log("b="+b);
-console.log("меняем местами:");
+alert("Допустим у нас есть переменные a и b:");
+prompt("a=",a);
+prompt("b=",b);
 b=a+b;
 a=b-a;
 b=b-a;
-console.log("a="+a);
-console.log("b="+b);
+alert("Меняем местами:\n\rТеперь a="+a+", b="+b);
+alert("Остальные части задания в консоли");
+admin="Василий";
+name=admin;
+console.log("name="+name);
+myTest=1000+"108";
+console.log("1000+'108'="+myTest);
+console.log("Тип переменной станет: "+typeof(myTest)+", т.к. при сложении литералов числового и строкового типов результат приводится к строковому типу");
+
